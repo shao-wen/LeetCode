293bae376e4a7ebee139086dc06785562f5ca471
shaowen <17689207868@163.com>
2021-01-26 19:11:54 +0800

    IsPowerOfTwo

--- IsPowerOfTwo.cpp
+++ IsPowerOfTwo.cpp
@@ -4,8 +4,8 @@ public:
     {
         if (n <= 0) {return false;}
         if (n == 1) {return true;}
-        while (n>1) {
-            if (n%2 !=0) {return false;}
+        while (n > 1) {
+            if (n % 2 !=0) {return false;}
             n = n / 2;
         }
         return true;
